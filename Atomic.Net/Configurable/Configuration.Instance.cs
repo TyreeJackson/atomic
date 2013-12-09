4ecd02923de151620e3e217bbc74594fad52e1ea
TyreeJackson <github@tyreejackson.com>
2013-12-08 19:47:34 -0600

    no changes

---
+++ Atomic.Net/Configurable/Configuration.Instance.cs
@@ -0,0 +1,33 @@
+﻿using AtomicNet;
+
+namespace AtomicNet
+{
+
+    public
+    abstract
+    partial
+    class       Configuration : Atom<Configuration>
+    {
+
+        public
+        class   Instance
+        {
+            public  List.KeyProperty    key;
+            public  string              Key             { get { return this.key; } set { this.key.SetValue(value); } }
+            public  string              SubClassKey;
+
+            public
+            class   List : IndexedList<List, string, Instance>
+            {
+                public  List() : base(a=>a.key)
+                {
+                    #warning NotImplemented
+                    throw new System.NotImplementedException();
+                }
+            }
+
+        }
+
+    }
+
+}
