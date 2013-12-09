9693c7a265c85bda66a0269663bb3e16c74ff2dc
TyreeJackson <github@tyreejackson.com>
2013-12-09 01:41:34 -0600

    Added missing file.

---
+++ Atomic.Net/Configurable/Configuration.Instance.cs
@@ -0,0 +1,40 @@
+﻿using AtomicNet;
+
+namespace AtomicNet
+{
+
+    public
+    abstract
+    partial
+    class       Configuration
+    {
+
+        public
+        class   InstanceConfiguration
+        {
+            public
+            readonly    List.KeyProperty    key;
+            public      string              Key             { get { return this.key; } set { this.key.SetValue(value); } }
+            public
+            readonly    string              SubclassKey;
+
+            public
+            class   List : IndexedList<List, string, InstanceConfiguration>
+            {
+                public  List() : base(a=>a.key)
+                {
+                    #warning NotImplemented
+                    throw new System.NotImplementedException();
+                }
+            }
+
+            protected   InstanceConfiguration(string subclassKey)
+            {
+                this.key            = new IndexedList<List,string,InstanceConfiguration>.KeyProperty(this);
+                this.SubclassKey    = subclassKey;
+            }
+        }
+
+    }
+
+}
