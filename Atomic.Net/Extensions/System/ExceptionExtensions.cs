ab4d009b54fd9a76e1537bb2bdb97c784ff2b7d7
TyreeJackson <github@tyreejackson.com>
2014-01-02 01:23:53 -0600

    Added missing ExceptionExtensions.cs file.

---
+++ Atomic.Net/Extensions/System/ExceptionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AtomicNet
+{
+
+    public
+    static  class   ExceptionExtensions
+    {
+
+        public  static  void    Throw(this Exception exception)
+        {
+            if (exception == null)  throw new Exception("An unknown error has occurred.");
+
+            System.Runtime.ExceptionServices
+            .ExceptionDispatchInfo.Capture
+            (
+                exception is PromiseException
+                ?   exception.InnerException
+                :   exception
+            ).Throw();
+        }
+
+    }
+
+}
