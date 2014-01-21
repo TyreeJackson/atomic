9b4f9b5bf2d7677f75f9d226da1772b9f58b121b
TyreeJackson <github@tyreejackson.com>
2014-01-21 00:14:29 -0600

    Added a few more Promise unit tests.

--- Atomic.Net.Tests/Support/PromiseTests.cs
+++ Atomic.Net.Tests/Support/PromiseTests.cs
@@ -29,6 +29,59 @@ namespace AtomicNetTests
         }
 
         [TestMethod]
+        public void WhenResolvingAPromiseSynchronouslyItShouldCallTheNextPromise()
+        {
+            bool    nextPromiseCalled   = false;
+            AtomicNet.Promise.Create
+            ((resolve,reject)=>
+            {
+                resolve();
+            })
+            .Then
+            (
+                ()=>AtomicNet.Promise.Create
+                ((resolve, reject)=>
+                {
+                    nextPromiseCalled   = true;
+                }),
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            );
+            Assert.AreEqual(true, nextPromiseCalled, "Next promise was not called synchronously.");
+        }
+
+        [TestMethod]
+        public void WhenResolvingAPromiseSynchronouslyItShouldCallTheNextPromiseAndThenTheResolver()
+        {
+            bool    nextPromiseCalled   = false;
+            bool    resolverCalled      = false;
+            AtomicNet.Promise.Create
+            ((resolve,reject)=>
+            {
+                resolve();
+            })
+            .Then
+            (
+                ()=>AtomicNet.Promise.Create
+                ((resolve, reject)=>
+                {
+                    nextPromiseCalled   = true;
+                    resolve();
+                }),
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            )
+            .WhenDone
+            (
+                ()=>
+                {
+                    resolverCalled  = true;
+                },
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            );
+            Assert.AreEqual(true,   nextPromiseCalled,  "Next promise was not called synchronously.");
+            Assert.AreEqual(true,   resolverCalled,     "WhenDone resolver was not called synchronously.");
+        }
+
+        [TestMethod]
         public void WhenResolvingAPromiseSynchronouslyItShouldCallTheRejectorIfAnExceptionIsThrownSynchronously()
         {
             AtomicNet.Promise.Create
@@ -94,6 +147,59 @@ namespace AtomicNetTests
                     Assert.IsInstanceOfType(((AtomicNet.PromiseException) ex).InnerException, typeof(InvalidOperationException), "An unexpected exception type was reported in the PromiseException: " + ((AtomicNet.PromiseException) ex).InnerException.GetType().FullName);
                 }
             );
+        }
+
+        [TestMethod]
+        public void WhenResolvingAPromiseOfIntSynchronouslyItShouldCallTheNextPromise()
+        {
+            int    nextValueShouldBe10  = 0;
+            AtomicNet.Promise<int>.Create
+            ((resolve,reject)=>
+            {
+                resolve(10);
+            })
+            .Then
+            (
+                value=>AtomicNet.Promise.Create
+                ((resolve, reject)=>
+                {
+                    nextValueShouldBe10 = value;
+                }),
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            );
+            Assert.AreEqual(10, nextValueShouldBe10, "WhenDone resolver was not called with the correct value.");
+        }
+
+        [TestMethod]
+        public void WhenResolvingAPromiseOfIntSynchronouslyItShouldCallTheNextPromiseAndThenTheResolver()
+        {
+            int     nextValueShouldBe10 = 0;
+            bool    resolverCalled      = false;
+            AtomicNet.Promise<int>.Create
+            ((resolve,reject)=>
+            {
+                resolve(10);
+            })
+            .Then
+            (
+                value=>AtomicNet.Promise.Create
+                ((resolve, reject)=>
+                {
+                    nextValueShouldBe10 = value;
+                    resolve();
+                }),
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            )
+            .WhenDone
+            (
+                ()=>
+                {
+                    resolverCalled  = true;
+                },
+                ex=>Assert.Fail("An unexpected exception was thrown: " + ex.Message)
+            );
+            Assert.AreEqual(10,     nextValueShouldBe10,    "WhenDone resolver was not called with the correct value.");
+            Assert.AreEqual(true,   resolverCalled,         "WhenDone resolver was not called synchronously.");
         }
 
     }
