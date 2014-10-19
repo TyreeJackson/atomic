e2e0b16869de30221d8a6e5ba815812a3bc0c039
TyreeJackson <github@tyreejackson.com>
2014-10-19 02:49:13 -0500

    temporary shelveset

--- AtomicWeb/demo-test/.js
+++ AtomicWeb/demo-test/.js
@@ -6,7 +6,31 @@ atomic.ready(function(global)
     var frameworkMock   = new atomic.mocking.Mock();
     frameworkMock.Setup(framework => framework.DownloadExists("2.0.0.0")).Returns("2.0.0.0");
 
-    console.info("====>   frameworkMock.Object.DownloadExists(\"2.0.0.0\") === \"2.0.0.0\": " + (frameworkMock.Object.DownloadExists("2.0.0.0") === "2.0.0.0"));
-    console.info("====>   Verify DownloadExists(\"2.0.0.0\") called once: " + frameworkMock.Verify(framework => framework.DownloadExists("2.0.0.0"), stat => stat.callCount === 1));
+    describe
+    (
+        "Download Exists", 
+        function()
+        {
+            it
+            (
+                "should return 2.0.0.0",
+                function()
+                {
+                    var result = frameworkMock.Object.DownloadExists("2.0.0.0");
+                    expect(result).toBe("2.0.0.0");
+                }
+            );
+        }
+    );
+
+    console.info
+    (
+        "====>   Verify DownloadExists(\"2.0.0.0\") called once: " + 
+        frameworkMock.Verify
+        (
+            framework   => framework.DownloadExists("2.0.0.0"), 
+            stat        => stat.callCount === 1
+        )
+    );
 
 }});
