05258cbaa4f8775c92e37f9a3f864e3338f9a1cd
TyreeJackson <github@tyreejackson.com>
2015-04-06 01:26:45 -0500

    Moved IISHttpHandler.AsyncResult class to its own file.

--- Atomic.Net/Host/IIS/IISHttpHandler.AsyncResult.cs
+++ Atomic.Net/Host/IIS/IISHttpHandler.AsyncResult.cs
@@ -10,6 +10,39 @@ namespace AtomicNet.IIS
     partial class IISHttpHandler
     {
 
+        public
+        sealed  class   AsyncResult : IAsyncResult
+        {
+
+            private     object              state;
+            private     object              stateLock                           = new object();
+            private     ManualResetEvent    resetEvent                          = null;
+            private     bool                completedSynchronously              = false;
+            private     bool                isCompleted                         = false;
+            private     AsyncCallback       asyncCallback                       = null;
+
+                        object              IAsyncResult.AsyncState             { get { return this.state; } }
+                        WaitHandle          IAsyncResult.AsyncWaitHandle        { get { lock(this.stateLock)    return this.resetEvent = this.resetEvent ?? new ManualResetEvent(false); } }
+                        bool                IAsyncResult.CompletedSynchronously { get { return this.completedSynchronously; } }
+                        bool                IAsyncResult.IsCompleted            { get { return this.isCompleted; } }
+
+            internal    void                CompleteRequest(bool completedSynchronously)
+            {
+                this.completedSynchronously = completedSynchronously;
+                this.isCompleted            = true;
+                lock(this.stateLock)    if (this.resetEvent != null)    this.resetEvent.Set();
+                try
+                {
+                    if (this.asyncCallback != null) this.asyncCallback(this);
+                }
+                catch(System.Exception ex)
+                {
+                    #warning Log this exception when the Logging Service is implemented
+                    throw;
+                }
+            }
+        }
+
     }
 
 }
