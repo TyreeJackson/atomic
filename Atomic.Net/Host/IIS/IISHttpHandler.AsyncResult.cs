7d30b7c8a8446d90817a51cadde294d1fd617fe7
TyreeJackson <github@tyreejackson.com>
2015-04-06 01:23:49 -0500

    Adding more unit tests.

---
+++ Atomic.Net/Host/IIS/IISHttpHandler.AsyncResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using AtomicNet;
+
+namespace AtomicNet.IIS
+{
+
+    partial class IISHttpHandler
+    {
+
+    }
+
+}
