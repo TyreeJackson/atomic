3113f2346d9146d349b9e19175c05d577b3cef6b
Tyree Jackson <github@tyreejackson.com>
2015-11-04 09:44:01 -0600

    New features and bug fixes.

--- AtomicJS/atomic/scripts/initializeViewAdapter.js
+++ AtomicJS/atomic/scripts/initializeViewAdapter.js
@@ -1,14 +1,20 @@
-﻿!function()
+!function()
 {"use strict";root.define("atomic.initializeViewAdapter",
 function(each)
 {
+    function cancelEvent(event)
+    {
+        event.preventDefault();
+        event.stopPropogation();
+        return false;
+    }
     var initializers    =
     {
-        onenter:    function(viewAdapter, callback) { viewAdapter.addEventListener("keypress", function(event){ if (event.keyCode==13) callback.call(viewAdapter); }, false); },
-        onescape:   function(viewAdapter, callback) { viewAdapter.addEventListener("keydown", function(event){ if (event.keyCode==27) callback.call(viewAdapter); }, false); },
+        onenter:    function(viewAdapter, callback) { viewAdapter.addEventListener("keypress", function(event){ if (event.keyCode==13) { callback.call(viewAdapter); return cancelEvent(event); } }, false); },
+        onescape:   function(viewAdapter, callback) { viewAdapter.addEventListener("keydown", function(event){ if (event.keyCode==27) { callback.call(viewAdapter); return cancelEvent(event); } }, false); },
         hidden:     function(viewAdapter, value)    { if (value) viewAdapter.hide(); }
     };
-    each(["bindAs", "bindSource", "bindTo", "onbind", "onunbind", "updateon"], function(val){ initializers[val] = function(viewAdapter, value) { viewAdapter["__" + val] = value; }; });
+    each(["bindAs", "bindSource", "bindTo", "onbind", "onshow", "onunbind", "updateon"], function(val){ initializers[val] = function(viewAdapter, value) { viewAdapter["__" + val] = value; }; });
     each(["blur", "change", "click", "contextmenu", "copy", "cut", "dblclick", "drag", "drageend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "focus", "focusin", "focusout", "input", "keydown", "keypress", "keyup", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "paste", "search", "select", "touchcancel", "touchend", "touchmove", "touchstart", "wheel"], function(val){ initializers["on" + val] = function(viewAdapter, callback) { viewAdapter.addEventListener(val, callback.bind(viewAdapter), false); }; });
 
     return function initializeViewAdapter(viewAdapter, viewAdapterDefinition)
