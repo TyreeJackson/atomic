53eeda48a720f97f2d63612e3843f79f23c11e17
Tyree Jackson <TyreeJackson@users.noreply.github.com>
2016-09-16 11:28:33 -0500

    Fixed a bug in readonly control in that occurs in Safari.

--- AtomicJS/atomic/scripts/html/readonly.js
+++ AtomicJS/atomic/scripts/html/readonly.js
@@ -4,7 +4,7 @@
     function readonly(elements, selector, parent)
     {
         control.call(this, elements, selector, parent);
-        Object.defineProperty(this, "__elements", {value: parent.__element.querySelectorAll(selector), configurable: true});
+        Object.defineProperty(this, "__elements", {value: Array.prototype.slice.call(parent.__element.querySelectorAll(selector)), configurable: true});
         this.__binder.defineDataProperties(this,
         {
             value:  {get: function(){return this.__element.innerHTML;}, set: function(value){var val = value&&value.isObserver?value():value; each(this.__elements, function(element){element.innerHTML = val;}); this.__element.innerHTML = val;}}
