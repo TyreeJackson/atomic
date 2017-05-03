a8fcd833e9572edb36b1dbea32ab5a077e434632
Tyree Jackson <tyreejackson@gmail.com>
2017-05-02 22:16:58 -0500

    interim commit before adding bind.when initializer feature.

--- AtomicJS/tutorial/3rdparty/atomic.js
+++ AtomicJS/tutorial/3rdparty/atomic.js
@@ -380,26 +380,48 @@
         Object.defineProperty(this, "__elements", {value: Array.prototype.slice.call(parent.__element.querySelectorAll(selector)), configurable: true});
         this.__binder.defineDataProperties(this,
         {
-            value:  {get: function(){return this.__element.innerHTML;}, set: function(value){var val = value&&value.isObserver?value():value; each(this.__elements, function(element){element.innerHTML = val;}); this.__element.innerHTML = val;}}
+            value:      {get: function(){return this.__element.innerHTML;}, set: function(value){var val = value&&value.isObserver?value():value; each(this.__elements, function(element){element.innerHTML = val;}); this.__element.innerHTML = val;}},
+            attributes:         
+            {
+                get:    function(){return this.__attributes;}, 
+                set:    function(value)
+                {
+                    if (value!==undefined&&value.isObserver) value=value(); 
+                    this.__attributes=value;
+
+                    if (value!==undefined)
+                    for(var key in value)
+                    {
+                        each(this.__elements, function(element){element.setAttribute("data-"+key, value[key]);});
+                        this.__element.setAttribute("data-" + key, value[key]);
+                    }
+                }
+            },
+            disabled:           {get: function(){return this.__element.disabled;},              set: function(value){each(this.__elements, function(element){element.disabled = !(!value);}); this.__element.disabled=!(!value);}},
+            display:            {get: function(){return this.__element.style.display=="";},     set: function(value){this[value?"show":"hide"]();}},
+            enabled:            {get: function(){return !this.__element.disabled;},             set: function(value){each(this.__elements, function(element){element.disabled = !value;}); this.__element.disabled=!value;}},
+            for:                {get: function(){return this.__element.getAttribute("for");},   set: function(value){each(this.__elements, function(element){element.setAttribute("for", value);}); this.__element.setAttribute("for", value);}}
         });
     }
     Object.defineProperty(readonly, "prototype", {value: Object.create(control.prototype)});
     Object.defineProperties(readonly.prototype,
     {
         constructor:    {value: readonly},
-        __createNode:   {value: function(){return document.createElement("span");}, configurable: true}
+        __createNode:   {value: function(){return document.createElement("span");}, configurable: true},
+        hide:               {value: function(){each(this.__elements, function(element){element.style.display="none";}); this.__element.style.display="none"; this.triggerEvent("hide"); return this;}},
+        show:               {value: function(){each(this.__elements, function(element){element.style.display="";}); this.__element.style.display=""; this.triggerEvent("show"); return this;}},
     });
     return readonly;
 });}();
 !function()
-{"use strict";root.define("atomic.html.link", function htmlLink(base)
+{"use strict";root.define("atomic.html.link", function htmlLink(base, each)
 {
     function link(elements, selector, parent)
     {
         base.call(this, elements, selector, parent);
         this.__binder.defineDataProperties(this,
         {
-            href: {get: function(){return this.__element.href;}, set: function(value){this.__element.href = value&&value.isObserver?value():value;}}
+            href: {get: function(){return this.__element.href;}, set: function(value){var val = value&&value.isObserver?value():value; each(this.__elements, function(element){element.href = val;}); this.__element.href = val;}}
         });
     }
     Object.defineProperty(link, "prototype", {value: Object.create(base.prototype)});
@@ -439,7 +461,9 @@
                     ?   "repeater"
                     :   element !== undefined
                         ?   multipleElements
-                            ?   "readonly"
+                            ?   element.nodeName.toLowerCase() == "a"
+                                ?   "link"
+                                :   "readonly"
                             :   elementControlTypes[element.nodeName.toLowerCase() + (element.type ? ":" + element.type.toLowerCase() : "")]||elementControlTypes[element.nodeName.toLowerCase()]||elementControlTypes.default
                         :   elementControlTypes.default);
     }
@@ -688,7 +712,7 @@
             "__templateElements":   {value: {}}
         });
         this.__binder.defineDataProperties(this, {value: {set: function(value)
-        {debugger;
+        {
             setTimeout
             (
                 (function(data)
@@ -1882,7 +1906,7 @@
 
     var control                 = new root.atomic.html.control(document, root.utilities.removeItemFromArray, window.setTimeout, each, eventsSet, dataBinder);
     var readonly                = new root.atomic.html.readonly(control, each);
-    var link                    = new root.atomic.html.link(readonly);
+    var link                    = new root.atomic.html.link(readonly, each);
     var container               = new root.atomic.html.container(control, each, viewAdapterFactory, new root.atomic.initializeViewAdapter(each));
     var panel                   = new root.atomic.html.panel(container, each);
     var composite               = new root.atomic.html.composite(container, each);
