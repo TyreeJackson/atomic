670ce33d2b7ca2c89da6f0c203fdc0adc15d0d20
Tyree Jackson <TyreeJackson@users.noreply.github.com>
2017-11-27 05:43:58 -0600

    Updated to the latest AtomicJS.

--- atomic-jstutorial/3rdparty/atomic.js
+++ atomic-jstutorial/3rdparty/atomic.js
@@ -510,9 +510,11 @@
         "input:checkbox":           "checkbox",
         "textarea":                 "input",
         "img":                      "image",
+        "audio":                    "audio",
         "select:select-multiple":   "multiselect",
         "select:select-one":        "select",
         "radiogroup":               "radiogroup",
+        "checkboxgroup":            "checkboxgroup",
         "a":                        "link",
         "label":                    "label"
     };
@@ -1122,23 +1124,6 @@
                 }
             }
         });
-        this.__element.addEventListener
-        (
-            "click", 
-            (function(event)
-            {
-                event=event||window.event; 
-                if (event.target.name === this.__name)
-                {
-                    this.value(getRadioGroupValue.call(this));
-                    this.triggerEvent("change");
-                }
-                event.cancelBubble=true;
-                if (event.stopPropagation) event.stopPropagation();
-                return false;
-            }).bind(this),
-            true
-        );
     }
     Object.defineProperty(radiogroup, "prototype", {value: Object.create(input.prototype)});
     Object.defineProperties(radiogroup.prototype,
@@ -1147,7 +1132,7 @@
         __createNode:       {value: function(){var element = document.createElement("radiogroup"); return element;}, configurable: true},
         count:              {get:   function(){ return this.__elements[0].options.length; }},
         selectedIndex:      {get:   function(){ return this.__elements[0].selectedIndex; },   set: function(value){ this.__element.selectedIndex=value; }},
-        __isValueSelected:  {value: function(value){return this.__rawValue === value;}},
+        __isValueSelected:  {value: function(value){return this.__rawValue === value;}}
     });
     each(["text","value"], function(name)
     {
@@ -1220,6 +1205,148 @@
     return multiselect;
 });}();
 !function()
+{"use strict";root.define("atomic.html.checkboxgroup", function htmlCheckboxGroup(input, dataBinder, each)
+{
+    function setCheckboxGroupValues(values)
+    {
+        if (typeof values === "function")   values = values();
+        if ( !Array.isArray(values)) values  = [values];
+        Object.defineProperty(this, "__rawValues", {value: values, configurable: true});
+        if (this.__options.length > 0) for(var counter=0;counter<this.__options.length;counter++) this.__options[counter].__checkboxElement.checked = values.indexOf(this.__options[counter].value()) > -1;
+        else
+        {
+            var options = this.__element.querySelectorAll("input[name='" + this.__name + "']");
+            for(var counter=0;counter<options.length;counter++) options[counter].__checkboxElement.checked = values.indexOf(options[counter].value()) > -1;
+        }
+    }
+    function getCheckboxGroupValues()
+    {
+        if (this.__options.length == 0) return this.__rawValues;
+        var values  = [];
+        if (this.__options.length > 0) for(var counter=0;counter<this.__options.length;counter++) if (this.__options[counter].__checkboxElement.checked) values.push(this.__options[counter].value());
+        else
+        {
+            var selectedOptions = this.__element.querySelectorAll("input[name='" + this.__name + "']:checked");
+            if (selectedOptions && selectedOptions.length > 0)  for(var counter=0;counter<selectedOptions.length;counter++) values.push(selectedOptions.value);
+        }
+
+        Object.defineProperty(this, "__rawValues", {value: values, configurable: true});
+        return values;
+    }
+    function captureTemplateIfNeeded()
+    {
+        if (this.__templateElement === undefined)
+        {
+            this.__checkboxButtonSelector	= this.__element.getAttribute("data-atomic-checkboxbutton")||"input[type='checkbox']";
+            this.__checkboxLabelSelector    = this.__element.getAttribute("data-atomic-checkboxlabel")||"label";
+            this.__templateElement          = this.__element.querySelector("checkboxgroupitem");
+            this.__templateElement.parentNode.removeChild(this.__templateElement);
+            clearCheckboxGroup(this.__element);
+        }
+    }
+    function checkboxoption(element, selector, name, parent, index)
+    {
+        Object.defineProperties(this, 
+        {
+            "parent":               {value: parent},
+            "__element":            {value: element},
+            "__sourceBinder":       {value: new dataBinder()},
+            "__parent":             {value: parent},
+            "__checkboxElement":    {value: element.querySelector(parent.__checkboxButtonSelector)},
+            "__checkboxLabel":      {value: element.querySelector(parent.__checkboxLabelSelector)},
+            "__text":               {value: null, configurable: true},
+            "__value":              {value: null, configurable: true}
+        });
+        this.__sourceBinder.defineDataProperties(this,
+        {
+            text:   {get: function(){return this.__text;}, set: function(value){Object.defineProperty(this,"__text",{value: value}); if (this.__checkboxLabel != null) this.__checkboxLabel.innerHTML = value&&value.isObserver?value():value;}},
+            value:  {get: function(){return this.__value;}, set: function(value){Object.defineProperty(this, "__value", {value: value}); if (this.__checkboxElement != null) this.__checkboxElement.value = value&&value.isObserver?value():value;}}
+        });
+        this.__checkboxElement.name = name;
+        this.__checkboxElement.id   = name+"-"+index;
+        this.__checkboxLabel.setAttribute("for", this.__checkboxElement.id);
+    }
+    Object.defineProperties(checkboxoption.prototype,
+    {
+        source:     {get: function(){return this.__sourceBinder.data;}, set: function(value){this.__sourceBinder.data = value;}},
+        selected:   {get: function(){return this.__checkboxElement.checked;}, set: function(value){this.__checkboxElement.checked = !(!value);}}
+    });
+    function createOption(sourceItem, index)
+    {
+        var option          = new checkboxoption(this.__templateElement.cloneNode(true), this.selector+"-"+index, this.__name, this, index);
+        option.text.bind    = this.optionText||"";
+        option.value.bind   = this.optionValue||"";
+        option.source       = sourceItem;
+        return option;
+    }
+    function checkboxgroup(elements, selector, parent)
+    {
+        input.call(this, elements, selector, parent);
+        Object.defineProperties(this, 
+        {
+            "__items":      {value: null, configurable: true},
+            "__options":    {value: []},
+            "__name":       {value: (this.__element.__selectorPath||"") + (this.__element.id||"unknown")}
+        });
+        this.__binder.defineDataProperties(this,
+        {
+            value:  {get: function(){return getCheckboxGroupValues.call(this);}, set: function(value){setCheckboxGroupValues.call(this, value===undefined?null:value);},  onchange: this.getEvents("change")},
+            items:
+            {
+                get:        function() {return this.__items;},
+                set:        function(value)
+                {
+                    Object.defineProperty(this, "__items", {value: value!==undefined&&value.isObserver?value():value, configurable: true});
+                    captureTemplateIfNeeded.call(this);
+
+                    bindCheckboxGroupSource.call(this, value);
+                }
+            }
+        });
+    }
+    Object.defineProperty(checkboxgroup, "prototype", {value: Object.create(input.prototype)});
+    Object.defineProperties(checkboxgroup.prototype,
+    {
+        constructor:        {value: checkboxgroup},
+        __createNode:       {value: function(){var element = document.createElement("checkboxgroup"); return element;}, configurable: true},
+        count:              {get:   function(){ return this.__elements[0].options.length; }},
+        selectedIndex:      {get:   function(){ return this.__elements[0].selectedIndex; },   set: function(value){ this.__element.selectedIndex=value; }},
+        __isValueSelected:  {value: function(value){return Array.isArray(this.__rawValues) && this.__rawValues.indexOf(value) > -1;}}
+    });
+    each(["text","value"], function(name)
+    {
+        var thisName    = name.substr(0,1).toUpperCase()+name.substr(1);
+        Object.defineProperty(checkboxgroup.prototype, "option"+thisName, 
+        {
+            get: function(){ return this["__option"+thisName]; },
+            set: function(value)
+            {
+                Object.defineProperty(this,"__option"+thisName, {value: value, configurable: true});
+                each(this.__options, function(option){option[name].bind = value;});
+            }
+        });
+    });
+    function clearCheckboxGroup(checkboxGroup){ for(var counter=checkboxGroup.childNodes.length-1;counter>=0;counter--) checkboxGroup.removeChild(checkboxGroup.childNodes[counter]); }
+    function rebindCheckboxGroupSource(){bindCheckboxGroupSource.call(this, this.__boundItems);}
+    function bindCheckboxGroupSource(items)
+    {
+        var selectedValue   = this.value();
+        clearCheckboxGroup(this.__element);
+        this.__options.splice(0, this.__options.length);
+        Object.defineProperty(this, "__boundItems", {value: items, configurable: true});
+        if (items === undefined)   return;
+        for(var counter=0;counter<items.count;counter++)
+        {
+            var sourceItem  = items.observe(counter);
+            var option      = createOption.call(this, sourceItem, counter);
+            this.__options.push(option);
+            this.__element.appendChild(option.__element);
+            option.selected = this.__isValueSelected(option.value());
+        }
+    }
+    return checkboxgroup;
+});}();
+!function()
 {"use strict";root.define("atomic.html.image", function htmlImage(control)
 {
     function image(elements, selector, parent)
@@ -1238,6 +1365,42 @@
         __createNode:   {value: function(){return document.createElement("image");}, configurable: true}
     });
     return image;
+});}();
+!function()
+{"use strict";root.define("atomic.html.audio", function htmlAudio(control)
+{
+    function audio(elements, selector, parent)
+    {
+        control.call(this, elements, selector, parent);
+        this.__binder.defineDataProperties(this,
+        {
+            autoplay:       {get: function(){return this.__element.autoplay},       set: function(value){this.__element.autoplay = value===true;}},
+            loop:           {get: function(){return this.__element.loop;},          set: function(value){this.__element.loop = value===true;}},
+            muted:          {get: function(){return this.__element.muted},          set: function(value){this.__element.muted = value===true;}},
+            nativeControls: {get: function(){return this.__element.controls;},      set: function(value){this.__element.controls = value===true;}},
+            preload:        {get: function(){return this.__element.preload;},       set: function(value){this.__element.preload = value===true;}},
+            alt:            {get: function(){return this.__element.alt;},           set: function(value){this.__element.alt = value||"";}},
+            mediaType:      {get: function(){return this.__element.type;},          set: function(value){this.__element.type = value||"";}},
+            value:          {get: function(){return this.__element.src;},           set: function(value){this.pause(); this.__element.src = value||""; this.triggerEvent("timeupdate"); }},
+            currentTime:    {get: function(){return this.__element.currentTime;},   set: function(value){this.__element.currentTime = value||0;},   onchange: this.getEvents("timeupdate")},
+            playbackRate:   {get: function(){return this.__element.playbackRate;},  set: function(value){this.__element.playbackRate = value||0;},  onchange: this.getEvents("ratechange")},
+            volume:         {get: function(){return this.__element.volume;},        set: function(value){this.__element.volume = value||0;},        onchange: this.getEvents("volumechange")}
+        });
+    }
+    Object.defineProperty(audio, "prototype", {value: Object.create(control.prototype)});
+    Object.defineProperties(audio.prototype,
+    {
+        constructor:    {value: audio},
+        __createNode:   {value: function(){return document.createElement("audio");}, configurable: true},
+        duration:       {value: function(){return this.__element.duration;}},
+        ended:          {value: function(){return this.__element.ended;}},
+        paused:         {value: function(){return this.__element.paused;}},
+        seeking:        {value: function(){return this.__element.seeking;}},
+        load:           {value: function(){this.__element.load();}},
+        play:           {value: function(){this.__element.play();}},
+        pause:          {value: function(){this.__element.pause();}}
+    });
+    return audio;
 });}();
 !function()
 {"use strict";root.define("atomic.html.button", function htmlButton(control)
@@ -1475,12 +1638,12 @@
         }},
         updateon:           {value: function(viewAdapter, value)    {if (Array.isArray(value))  viewAdapter.updateon = value;}}
     });
-    each(["value"], function(val){ initializers[val] = function(viewAdapter, value) { if (viewAdapter[val] === undefined) {console.error("property named " +val + " was not found on the view adapter of type " + typeof(viewAdapter) + ".  Skipping initializer."); return;} viewAdapter[val](value); }; });
+    each(["alt", "autoplay", "currentTime", "loop", "muted", "nativeControls", "preload", "mediaType", "playbackRate", "value", "volume"], function(val){ initializers[val] = function(viewAdapter, value) { if (viewAdapter[val] === undefined) {console.error("property named " +val + " was not found on the view adapter of type " + viewAdapter.constructor.name + ".  Skipping initializer."); return;} viewAdapter[val](value); }; });
     each(["optionValue", "optionText", "isDataRoot"], function(val){ initializers[val] = function(viewAdapter, value) { viewAdapter[val] = value; }; });
     initializers.classes = function(viewAdapter, value) { each(value, function(val){viewAdapter.toggleClass(val, true);}); };
     each(["onbind", "ondataupdate", "onsourceupdate", "onunbind"], function(val){ initializers[val] = function(viewAdapter, value) { viewAdapter["__" + val] = value; }; });
     each(["show", "hide"], function(val){ initializers["on"+val] = function(viewAdapter, callback) { viewAdapter.addEventListener(val, function(event){ callback.call(viewAdapter); }, false, true); }; });
-    each(["blur", "change", "click", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "focus", "focusin", "focusout", "input", "keydown", "keypress", "keyup", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "paste", "search", "select", "touchcancel", "touchend", "touchmove", "touchstart", "wheel", "transitionend"], function(val)
+    each(["abort", "blur", "canplay", "canplaythrough", "change", "click", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchanged", "ended", "error", "focus", "focusin", "focusout", "input", "loadeddata", "loadedmetadata", "loadstart", "keydown", "keypress", "keyup", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "paste", "pause", "play", "playing", "progress", "ratechange", "search", "seeked", "seeking", "select", "stalled", "suspend", "timeupdate", "touchcancel", "touchend", "touchmove", "touchstart", "volumechange", "waiting", "wheel", "transitionend"], function(val)
     {
         initializers["on" + val] = function(viewAdapter, callback) { viewAdapter.addEventListener(val, callback.bind(viewAdapter), false); };
     });
@@ -2794,8 +2957,10 @@
     var checkbox                = new root.atomic.html.checkbox(control);
     var select                  = new root.atomic.html.select(input, dataBinder, each);
     var radiogroup              = new root.atomic.html.radiogroup(input, dataBinder, each);
+    var checkboxgroup           = new root.atomic.html.checkboxgroup(input, dataBinder, each);
     var multiselect             = new root.atomic.html.multiselect(select);
     var image                   = new root.atomic.html.image(control);
+    var audio                   = new root.atomic.html.audio(control);
     var button                  = new root.atomic.html.button(control);
 
     Object.defineProperties(controlTypes,
@@ -2813,8 +2978,10 @@
         checkbox:       {value: checkbox},
         select:         {value: select},
         radiogroup:     {value: radiogroup},
+        checkboxgroup:  {value: checkboxgroup},
         multiselect:    {value: multiselect},
         image:          {value: image},
+        audio:          {value: audio},
         button:         {value: button}
     });
     var atomic  = { viewAdapterFactory: viewAdapterFactory, observer: observer };
