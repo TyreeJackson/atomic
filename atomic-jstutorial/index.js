f1fec1ceda0b01008b02bb04f339deb7b568fcbd
Tyree Jackson <tyreejackson@gmail.com>
2019-02-25 23:47:58 -0600

    Added blocked popup check to fix a null reference error.  Modified the debugInfo code a little.

--- atomic-jstutorial/index.js
+++ atomic-jstutorial/index.js
@@ -4,9 +4,16 @@ function ComposeApp()
     var atomic                      = root.atomic.html.compositionRoot(undefined, true);
     var debuggerWindow              = window.open(undefined, "debugInfo", "menubar=no,status=no,toolbar=no");
     var debugInfoPanel              = document.querySelector("#debugInfoPanel");
-    debuggerWindow.document.open();
-    debuggerWindow.document.write(debugInfoPanel.outerHTML);
-    debuggerWindow.document.close();
+    if(debuggerWindow)
+    {
+        debuggerWindow.document.open();
+        debuggerWindow.document.write(debugInfoPanel.outerHTML);
+        debuggerWindow.document.close();
+    }
+    else
+    {
+        alert("Pop ups are blocked.  If you want to see the app structure information window, please allow popups for this page and try again.");
+    }
     debugInfoPanel.parentNode.removeChild(debugInfoPanel);
     var app                         =
     new root.atomic.tutorial.appController
@@ -18,7 +25,7 @@ function ComposeApp()
         ),
         new root.atomic.tutorial.appProxy(window.localStorage, root.utilities.removeFromArray),
         new root.path(),
-        atomic.viewAdapterFactory.createView(new root.atomic.tutorial.debugInfoViewer.appView(atomic.debugInfoObserver), debuggerWindow.document.querySelector("#debugInfoPanel"))
+        debuggerWindow ? atomic.viewAdapterFactory.createView(new root.atomic.tutorial.debugInfoViewer.appView(atomic.debugInfoObserver), debuggerWindow.document.querySelector("#debugInfoPanel")) : undefined
     );
     root.atomic.init({debugInfoObserver: atomic.debugInfoObserver});
     app.launch();
